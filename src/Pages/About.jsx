01b9730ab1766d3a8511bdb66ec5d074d35e6937
MusabBaig123 <2123060@brunel.ac.uk>
2025-11-24 05:57:01 +0000

    Delete src directory

--- src/Pages/About.jsx
+++
@@ -1,3 +0,0 @@
-export default function About() {
-  return <h1 style={{ textAlign: "center", marginTop: "40px" }}>About Page</h1>;
-}
--- src/Pages/Contact.jsx
+++
@@ -1,3 +0,0 @@
-export default function Contact() {
-  return <h1 style={{ textAlign: "center", marginTop: "40px" }}>Contact Page</h1>;
-}
